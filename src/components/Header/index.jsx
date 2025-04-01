0495c3fad7f779f237d67141faace2dea4248950
AqibNiazi <aqibjaved5201@gmail.com>
2025-04-01 22:53:19 +0500

    Readme file added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,10 @@
-import React from "react";
 import logo from "../../assets/investment-calculator-logo.png";
-const Header = () => {
+
+export default function Header() {
   return (
     <header id="header">
-      <img src={logo} alt="Money bag logo" />
+      <img src={logo} alt="Logo showing a money bag" />
       <h1>Investment Calculator</h1>
     </header>
   );
-};
-
-export default Header;
+}
